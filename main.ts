9cdec17c2eceaf511cdbfce511a587fc911605e6
Clebost <61158869+Clebost@users.noreply.github.com>
2020-06-28 19:35:34 +0200

    Mise en place du servo moteur.

--- main.ts
+++ main.ts
@@ -2,5 +2,7 @@ basic.forever(function () {
     if (true) {
         makerbit.showStringOnLcd1602("Binevenue chez :", makerbit.position1602(LcdPosition1602.P0), 16)
         makerbit.showStringOnLcd1602("MakerBit", makerbit.position1602(LcdPosition1602.P16), 16)
+        pins.servoSetPulse(AnalogPin.P0, 1500)
+        pins.servoSetPulse(AnalogPin.P0, -1500)
     }
 })
