d24047a1482f8c251867f177a6332e932f0f1fab
Clebost <61158869+Clebost@users.noreply.github.com>
2020-06-28 19:24:16 +0200

    Mise en place d l'afficheur LCD I2C

--- main.ts
+++ main.ts
@@ -1,3 +1,6 @@
 basic.forever(function () {
-	
+    if (true) {
+        makerbit.showStringOnLcd1602("Binevenue chez :", makerbit.position1602(LcdPosition1602.P0), 16)
+        makerbit.showStringOnLcd1602("MakerBit", makerbit.position1602(LcdPosition1602.P16), 16)
+    }
 })
