e5fe25eaf8ae515a7bd5c8913e3700bb4b819322
Cleboost <61158869+Clebost@users.noreply.github.com>
2020-06-29 09:47:03 +0200

    Correction bug LED

--- main.ts
+++ main.ts
@@ -1,10 +1,10 @@
 let temp = 0
 input.onButtonPressed(Button.A, function () {
+    temp = 1
     makerbit.showStringOnLcd1602("Binevenue chez :", makerbit.position1602(LcdPosition1602.P0), 16)
     makerbit.showStringOnLcd1602("MakerBit", makerbit.position1602(LcdPosition1602.P16), 16)
-    temp = 0
     pins.servoWritePin(AnalogPin.P0, 1500)
-    while (temp == 0) {
+    for (let index = 0; index < 2; index++) {
         tinkercademy.LED(DigitalPin.P1, OnOff.On)
         basic.pause(1000)
         tinkercademy.LED(DigitalPin.P1, OnOff.Off)
@@ -13,8 +13,8 @@ input.onButtonPressed(Button.A, function () {
 })
 input.onButtonPressed(Button.B, function () {
     pins.servoWritePin(AnalogPin.P0, 0)
-    temp = 1
-    for (let index = 0; index < 2; index++) {
+    temp = 0
+    while (temp == 0) {
         tinkercademy.LED(DigitalPin.P1, OnOff.On)
         basic.pause(1000)
         tinkercademy.LED(DigitalPin.P1, OnOff.Off)
