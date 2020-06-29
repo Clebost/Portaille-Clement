a0163f4a68f10b36943ba57cf13d083949293a43
Cleboost <61158869+Clebost@users.noreply.github.com>
2020-06-29 11:54:06 +0200

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -5,9 +5,9 @@ input.onButtonPressed(Button.A, function () {
     makerbit.showStringOnLcd1602("MakerBit", makerbit.position1602(LcdPosition1602.P16), 16)
     pins.servoWritePin(AnalogPin.P0, 1500)
     for (let index = 0; index < 2; index++) {
-        tinkercademy.LED(DigitalPin.P1, OnOff.On)
+        tinkercademy.LED(DigitalPin.P3, OnOff.On)
         basic.pause(1000)
-        tinkercademy.LED(DigitalPin.P1, OnOff.Off)
+        tinkercademy.LED(DigitalPin.P3, OnOff.Off)
         basic.pause(1000)
     }
 })
@@ -15,9 +15,12 @@ input.onButtonPressed(Button.B, function () {
     pins.servoWritePin(AnalogPin.P0, 0)
     temp = 0
     while (temp == 0) {
-        tinkercademy.LED(DigitalPin.P1, OnOff.On)
+        tinkercademy.LED(DigitalPin.P4, OnOff.On)
         basic.pause(1000)
-        tinkercademy.LED(DigitalPin.P1, OnOff.Off)
+        tinkercademy.LED(DigitalPin.P4, OnOff.Off)
         basic.pause(1000)
     }
+})
+basic.forever(function () {
+    tinkercademy.LED(DigitalPin.P3, OnOff.Off)
 })
